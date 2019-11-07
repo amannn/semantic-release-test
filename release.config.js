148aad536e2c21533b9209e60c9328833d089e6d
Jan Amann <jan@amann.me>
2019-11-07 17:34:24 +0100

    fix!: Update commit-analyzer

--- release.config.js
+++ release.config.js
@@ -1,19 +1,6 @@
 module.exports = {
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        parserOpts: {
-          headerPattern: /^(\w*!?)(?:\(([\w\$\.\-\* ]*)\))?\: (.*)$/
-        },
-        releaseRules: [
-          {type: 'fix', release: 'patch'},
-          {type: 'feat', release: 'minor'},
-          {type: 'fix!', release: 'major'},
-          {type: 'feat!', release: 'major'}
-        ]
-      }
-    ],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', {changelogTitle: '# Changelog'}],
     ['@semantic-release/npm', {npmPublish: false}],

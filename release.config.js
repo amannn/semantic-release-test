8593eb0567dcea4ba86ca063d969cd9de358a8bb
Jan Amann <jan@amann.me>
2019-11-08 08:37:18 +0100

    fix!: Attempt at configuring preset at the top level.

--- release.config.js
+++ release.config.js
@@ -1,6 +1,7 @@
 module.exports = {
+  preset: 'conventionalcommits',
   plugins: [
-    ['@semantic-release/commit-analyzer', {preset: 'conventionalcommits'}],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/changelog', {changelogTitle: '# Changelog'}],
     ['@semantic-release/npm', {npmPublish: false}],
